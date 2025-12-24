2821501731be362b48e491140a673f551a866cec
Zayed891 <jayedaktar35@gmail.com>
2025-12-24 14:37:07 +0530

    Add CORS configuration for frontend

---
+++ backend/config/cors.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure CORS settings for your application. This
+    | determines which cross-origin requests can access your API.
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://beyond-chat-lac.vercel.app',
+        'http://localhost:3000',
+        'http://localhost:5173',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
